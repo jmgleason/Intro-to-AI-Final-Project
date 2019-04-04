d2954287118974a41acecaffaf598b7c5b61ad31
Juri Ando <jando16407c@gmail.com>
2019-04-03 21:21:08 -0400

    PrintGrid() fixed

--- Backtracking_Algorithm/Backtracking.hpp
+++ Backtracking_Algorithm/Backtracking.hpp
@@ -1,37 +1,70 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include "Queen.h"
 
 //Struct to store a 2D array of Queen grid
 struct QueenGrid {
     Queen Grid[numOfQueens][numOfQueens];
+    bool fail;
 }; 
 
 
 //Recursive function for the CSP Backtracking algorithm
-QueenGrid CSP_Backtracking(QueenGrid grid){
+QueenGrid CSP_Backtracking(QueenGrid grid, std::vector<Queen> queenPos ){
+    /* If passed grid is complete then return */
+    //check each queen's confliction when number of queen is 8, otherwise skip and continue
+    if( queenPos.size() == 8 ){
+        //iterate and check if there's no confliction
+        for( std::vector<Queen>::iterator it=queenPos.begin(); it != queenPos.end(); ++it ){
+            if( calculateConflicts( *it, grid.Grid ) > 0 ){
+                //not complete so return failure
+                grid.fail = true;
+                return grid;
+            }
+        }
+        //grid is complete so return it
+        grid.fail = false;
+        return grid;
+    }
+    else if( queenPos.size() < 8 ){
+        //select unassigned row
+        int currrentRow = queenPos.size();
+        return grid;
+    }
+    
+    
     return grid;
 };
 
 
 //Display NxN queen grid
 void PrintGrid( QueenGrid grid ){
-    for( int x=0; x<numOfQueens; ++x ){
-        if( x==0 ){ std::cout<<"|"; for( int i=0; i<numOfQueens-1; ++i ){std::cout<<"---+";} std::cout<<"---|\n"; }
+
+    //x-axis interation
+    for( int y=numOfQueens-1; y>=0; --y ){
+        
+        //Display the first top border
+        if( y==numOfQueens-1 ){
+            std::cout<<"|";
+            for( int i=0; i<numOfQueens-1; ++i ){std::cout<<"---+";} 
+            std::cout<<"---|\n"; 
+        }
+
+        //y-axis iteration -> Display 'X' if Queen is active, else blank
         std::cout<<"|";
-        for( int y=numOfQueens-1; y>=0; --y ){
+        for( int x=0; x<numOfQueens; ++x ){
             if( grid.Grid[x][y].active == true ){
-                std::cout<<" X ";
+                std::cout<<" X "<<"|";
             }
             else {
-                std::cout<<"   ";
+                std::cout<<"   "<<"|";
             }
-            std::cout<<"|";
         }
-        std::cout<<"\n";
-        std::cout<<"|";
+
+        //Display the bottom border
+        std::cout<<"\n"<<"|";
         for( int i=0; i<numOfQueens-1; ++i ){std::cout<<"---+";}
-        std::cout<<"---|";
-        std::cout<<"\n";
+        std::cout<<"---|"<<"\n";
     }
 }
